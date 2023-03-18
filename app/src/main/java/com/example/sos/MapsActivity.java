d576e6b335143a7a3d0f2ad1cfc57ff64c67d7a4
Piyusharma1 <psharma413512@gmail.com>
2023-03-18 15:27:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sos/MapsActivity.java
@@ -0,0 +1,302 @@
+package com.example.sos;
+
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.example.sos.ContactModel;
+import com.example.sos.CustomAdapter;
+import com.example.sos.DbHelper;
+import com.example.sos.ReactivateService;
+import com.example.sos.SensorService;
+
+import java.util.List;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class MapsActivity extends AppCompatActivity {
+
+
+    private static final int IGNORE_BATTERY_OPTIMIZATION_REQUEST = 1002;
+    private static final int PICK_CONTACT = 1;
+
+    // create instances of various classes to be used
+    Button button1, button2, button3,button4;
+
+    ViewFlipper flipper;
+    ListView listView;
+    DbHelper db;
+    List<ContactModel> list;
+    CustomAdapter customAdapter;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //button used to new window
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(view -> {
+            Intent secondActivityIntent = new Intent(
+                    getApplicationContext(), showcontact.class
+            );
+            startActivity(secondActivityIntent);
+        });
+
+        int imgarry[]={R.drawable.a1,R.drawable.a2,R.drawable.a3};
+        flipper=(ViewFlipper)findViewById(R.id.flipper);
+
+        for (int i=0;i<imgarry.length;i++)
+        {
+            showimage(imgarry[i]);
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // check for runtime permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.READ_CONTACTS}, 100);
+            }
+        }
+
+        // this is a special permission required only by devices using
+        // Android Q and above. The Access Background Permission is responsible
+        // for populating the dialog with "ALLOW ALL THE TIME" option
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, 100);
+        }
+
+        // check for BatteryOptimization,
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (pm != null && !pm.isIgnoringBatteryOptimizations(getPackageName())) {
+                askIgnoreOptimization();
+            }
+        }
+
+        // start the service
+        SensorService sensorService = new SensorService();
+        Intent intent = new Intent(this, sensorService.getClass());
+        if (!isMyServiceRunning(sensorService.getClass())) {
+            startService(intent);
+        }
+
+
+        button1 = findViewById(R.id.Button1);
+        listView = (ListView) findViewById(R.id.ListView);
+        db = new DbHelper(this);
+        list = db.getAllContacts();
+        customAdapter = new CustomAdapter(this, list);
+        listView.setAdapter(customAdapter);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling of getContacts()
+                if (db.count() != 5) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(intent, PICK_CONTACT);
+                } else {
+                    Toast.makeText(MapsActivity.this, "Can't Add more than 5 Contacts", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //add contact in police
+        button2 = findViewById(R.id.Button2);
+        listView = (ListView) findViewById(R.id.ListView);
+        db = new DbHelper(this);
+        list = db.getAllContacts();
+        customAdapter = new CustomAdapter(this, list);
+        listView.setAdapter(customAdapter);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling of getContacts()
+                if (db.count() != 5) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(intent, PICK_CONTACT);
+                } else {
+                    Toast.makeText(MapsActivity.this, "Can't Add more than 5 Contacts", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        //add contact in hospital
+        button3 = findViewById(R.id.Button3);
+        listView = (ListView) findViewById(R.id.ListView);
+        db = new DbHelper(this);
+        list = db.getAllContacts();
+        customAdapter = new CustomAdapter(this, list);
+        listView.setAdapter(customAdapter);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling of getContacts()
+                if (db.count() != 5) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    startActivityForResult(intent, PICK_CONTACT);
+                } else {
+                    Toast.makeText(MapsActivity.this, "Can't Add more than 5 Contacts", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    //////////////////////////////////
+    public void showimage(int img)
+    {
+        ImageView Imageview=new ImageView(this);
+        Imageview.setBackgroundResource(img);
+        flipper.addView(Imageview);
+        flipper.setFlipInterval(5000);
+        flipper.setAutoStart(true);
+        flipper.setInAnimation(this, android.R.anim.slide_out_right);
+        flipper.setOutAnimation(this, android.R.anim.slide_in_left);
+
+
+    }
+    /////////////////////////////////
+
+    // method to check if the service is running
+    private boolean isMyServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                Log.i("Service status", "Running");
+                return true;
+            }
+        }
+        Log.i("Service status", "Not running");
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Intent broadcastIntent = new Intent();
+        broadcastIntent.setAction("restartservice");
+        broadcastIntent.setClass(this, ReactivateService.class);
+        this.sendBroadcast(broadcastIntent);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(this, "Permissions Denied!\n Can't use the App!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @SuppressLint("Range")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // get the contact from the PhoneBook of device
+        switch (requestCode) {
+            case (PICK_CONTACT):
+                if (resultCode == Activity.RESULT_OK) {
+
+                    Uri contactData = data.getData();
+                    Cursor c = managedQuery(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+
+                        String id = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts._ID));
+                        String hasPhone = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        String phone = null;
+                        try {
+                            if (hasPhone.equalsIgnoreCase("1")) {
+                                Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+                                phones.moveToFirst();
+                                phone = phones.getString(phones.getColumnIndex("data1"));
+                            }
+                            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                            db.addcontact(new ContactModel(0, name, phone));
+                            list = db.getAllContacts();
+                            customAdapter.refresh(list);
+                        } catch (Exception ex) {
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    // this method prompts the user to remove any
+    // battery optimisation constraints from the App
+    private void askIgnoreOptimization() {
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            @SuppressLint("BatteryLife") Intent intent = new Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivityForResult(intent, IGNORE_BATTERY_OPTIMIZATION_REQUEST);
+        }
+
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/sos/showcontact.java
@@ -0,0 +1,16 @@
+package com.example.sos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showcontact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showcontact);
+
+
+    }
+}
